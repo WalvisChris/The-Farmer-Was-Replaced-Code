6d6b40eb2bc4db31865fab4729ca87e182897586
WalvisChris <chris.zuidervaart@gmail.com>
2026-09-08 23:44:35 +0200

    clones

--- Wood_Clones.py
+++ Wood_Clones.py
@@ -3,10 +3,14 @@ total_drones = 7
 def CutLane():
 	for _ in range(get_world_size()):
 		harvest()
+		if (get_pos_x() + get_pos_y()) % 2 == 0:
+			plant(Entities.Tree)
+		else:
+			plant(Entities.Bush)
 		move(East)
 	
 clear()
-set_world_size(total_drones)
+set_world_size(total_drones*2)
 while True:
 	if total_drones - num_drones() > 0:
 		spawn_drone(CutLane)
